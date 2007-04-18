b909135d1e88a87e44e2db9f9fb4eebb59a8a587
Martin Blom <martin@blom.org>
2007-04-18 19:07:05 +0000

    Misc.

--- src/org/blom/martin/esxx/js/JSURI.java
+++ src/org/blom/martin/esxx/js/JSURI.java
@@ -27,41 +27,6 @@ public class JSURI
       return "URI";
     }
 
-
-    public static Object jsFunction_load(Context cx, Scriptable thisObj,
-					 Object[] args, Function funObj)
-      throws IOException, org.xml.sax.SAXException {
-      JSURI  js_this = checkInstance(thisObj);
-      String type    = "text/xml";
-      HashMap<String,String> params = new HashMap<String,String>();
-
-      if (args.length >= 1 && args[0] != Context.getUndefinedValue()) {
-	type = parseMIMEType(Context.toString(args[0]), params);
-      }
-
-      return js_this.load(cx, thisObj, type, params);
-    }
-
-
-    public static Object jsFunction_query(Context cx, Scriptable thisObj,
-					  Object[] args, Function funObj)
-      throws SQLException, IOException, org.xml.sax.SAXException
-    {
-      JSURI  js_this = checkInstance(thisObj);
-      String type    = "text/xml";
-      HashMap<String,String> params = new HashMap<String,String>();
-
-      if (args.length < 1 || args[0] == Context.getUndefinedValue()) {
-	throw Context.reportRuntimeError("Missing query argument"); 
-      }
-
-      if (args.length >= 2 && args[1] != Context.getUndefinedValue()) {
-	type = parseMIMEType(Context.toString(args[1]), params);
-      }
-
-      return js_this.query(cx, thisObj, Context.toString(args[0]), type, params);
-    }
-
     static public Object jsConstructor(Context cx, 
 				       java.lang.Object[] args, 
 				       Function ctorObj, 
@@ -98,13 +63,53 @@ public class JSURI
       return null;
     }
 
+    public static Object jsFunction_load(Context cx, Scriptable thisObj,
+					 Object[] args, Function funObj)
+      throws IOException, org.xml.sax.SAXException {
+      JSURI  js_this = checkInstance(thisObj);
+      String type    = null;
+      HashMap<String,String> params = new HashMap<String,String>();
+
+      if (args.length >= 1 && args[0] != Context.getUndefinedValue()) {
+	type = parseMIMEType(Context.toString(args[0]), params);
+      }
+
+      return js_this.load(cx, thisObj, type, params);
+    }
+
+
+    public static Object jsFunction_query(Context cx, Scriptable thisObj,
+					  Object[] args, Function funObj)
+      throws SQLException, IOException, org.xml.sax.SAXException
+    {
+      JSURI  js_this = checkInstance(thisObj);
+      String type    = null;
+      HashMap<String,String> params = new HashMap<String,String>();
+
+      if (args.length < 1 || args[0] == Context.getUndefinedValue()) {
+	throw Context.reportRuntimeError("Missing query argument"); 
+      }
+
+      if (args.length >= 2 && args[1] != Context.getUndefinedValue()) {
+	type = parseMIMEType(Context.toString(args[1]), params);
+      }
+
+      return js_this.query(cx, thisObj, Context.toString(args[0]), type, params);
+    }
+
+
     public String toString() {
       return uri.toString();
     }
 
+
     private Object load(Context cx, Scriptable thisObj, 
 			String type, HashMap<String,String> params)
       throws IOException, org.xml.sax.SAXException {
+      if (type == null) {
+	type = "text/xml";
+      }
+
       if (type.equals("text/xml")) {
 	Document result = null;
 
@@ -172,15 +177,15 @@ public class JSURI
 	}
 	else if (f.isFile()) {
 	  element = result.createElement("file");
-	  setParam(result, element, "length", Long.toString(f.length()));
+	  addChild(element, "length", Long.toString(f.length()));
 	}
 	      
-	setParam(result, element, "name", f.getName());
-	setParam(result, element, "path", f.getPath());
-	setParam(result, element, "uri", f.toURI().toString());
-	setParam(result, element, "isHidden", f.isHidden() ? "true" : "false");
-	setParam(result, element, "lastModified", Long.toString(f.lastModified()));
-	setParam(result, element, "id", Integer.toHexString(f.hashCode()));
+	addChild(element, "name", f.getName());
+	addChild(element, "path", f.getPath());
+	addChild(element, "uri", f.toURI().toString());
+	addChild(element, "hidden", f.isHidden() ? "true" : "false");
+	addChild(element, "lastModified", Long.toString(f.lastModified()));
+	addChild(element, "id", Integer.toHexString(f.hashCode()));
 	root.appendChild(element);
       }
 
@@ -194,6 +199,20 @@ public class JSURI
       String scheme = uri.getScheme();
       
       if (scheme.equals("jdbc")) {
+	return queryJDBC(cx, thisObj, query, type, params);
+      }
+      else {
+	throw Context.reportRuntimeError("URI protocol '" + scheme + 
+					 "' does not support queries."); 
+      }
+    }
+
+
+    private Object queryJDBC(Context cx, Scriptable thisObj,
+			     String query, String type, HashMap<String,String> params)
+      throws SQLException {
+      
+      if (type == null || type.equals("text/x-sql")) {
 	Properties properties = new Properties();
 
 	for (Object id : ScriptableObject.getPropertyIds(thisObj)) {
@@ -226,7 +245,7 @@ public class JSURI
 	    Element row = result.createElement("row");
 	    
 	    for (int i = 0; i < count; ++i) {
-	      setParam(result, row, names[i], rs.getString(i + 1));
+	      addChild(row, names[i], rs.getString(i + 1));
 	    }
 
 	    root.appendChild(row);
@@ -239,18 +258,21 @@ public class JSURI
 	}
       }
       else {
-	throw Context.reportRuntimeError("URI protocol '" + scheme + 
-					 "' does not support queries."); 
+	throw Context.reportRuntimeError("JDBC query type '" + type + 
+					 "' not supported."); 
       }
     }
 
 
-    private void setParam(Document document, Element element, String name, String value) {
+    private static void addChild(Element element, String name, String value) {
+      Document document = element.getOwnerDocument();
+
 //      element.setAttribute(name, value);
       Element e = document.createElement(name);
       e.appendChild(document.createTextNode(value));
       element.appendChild(e);
     }
+
 
     private static String parseMIMEType(String ct, HashMap<String,String> params) {
       String[] parts = ct.split(";");
