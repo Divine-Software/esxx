d8efed6be846f13c43b5f415773adb8d7846b92a
Martin Blom <martin@blom.org>
2007-05-26 20:03:41 +0000

    Extended with cause and status code.

--- src/org/blom/martin/esxx/ESXXException.java
+++ src/org/blom/martin/esxx/ESXXException.java
@@ -4,6 +4,28 @@ package org.blom.martin.esxx;
 public class ESXXException 
   extends Exception {
     public ESXXException(String why) { 
-      super(why); 
+      super(why);
+      statusCode = 500;
     }
+
+    public ESXXException(String why, Throwable cause) { 
+      super(why, cause); 
+      statusCode = 500;
+    }
+
+    public ESXXException(int status, String why) { 
+      super(why); 
+      statusCode = status;
+    }
+
+    public ESXXException(int status, String why, Throwable cause) { 
+      super(why, cause); 
+      statusCode = status;
+    }
+
+    public int getStatus() {
+      return statusCode;
+    }
+
+    private int statusCode;
 }
