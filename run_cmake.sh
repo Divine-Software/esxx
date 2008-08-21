eb40b81dbf40eea30a1686e8be3e5d5746df2875
Martin Blom <martin@blom.org>
2008-08-21 14:44:20 +0000

    Set JAVA_HOME on OSX.

--- run_cmake.sh
+++ run_cmake.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+
 LIB_SUFFIX=
 
 test -d /lib64 && LIB_SUFFIX=64
+
+if [ "$OSTYPE" == "darwin9.0" ]; then
+    export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home
+fi
 
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo \
       -DLIB_SUFFIX=$LIB_SUFFIX \
