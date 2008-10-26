c38ba156b8901120c66ccce96142c00aeebbd6d8
Martin Blom <martin@blom.org>
2008-10-26 21:55:50 +0000

    Added HTTP status codes.

--- src/org/esxx/js/JSResponse.java
+++ src/org/esxx/js/JSResponse.java
@@ -77,8 +77,71 @@ public class JSResponse
     return new JSResponse(status, headers, result, content_type);
   }
 
-  @Override
-  public String getClassName() {
+  public static void finishInit(Scriptable scope, 
+				FunctionObject constructor,
+				Scriptable prototype) {
+
+    int flags = ScriptableObject.CONST | ScriptableObject.PERMANENT | ScriptableObject.READONLY;
+
+    constructor.defineProperty("CONTINUE",                        100, flags);
+    constructor.defineProperty("SWITCHING_PROTOCOLS",             101, flags);
+    constructor.defineProperty("PROCESSING",                      102, flags);
+    constructor.defineProperty("OK",                              200, flags);
+    constructor.defineProperty("CREATED",                         201, flags);
+    constructor.defineProperty("ACCEPTED",                        202, flags);
+    constructor.defineProperty("NON_AUTHORITATIVE_INFORMATION",   203, flags);
+    constructor.defineProperty("NO_CONTENT",                      204, flags);
+    constructor.defineProperty("RESET_CONTENT",                   205, flags);
+    constructor.defineProperty("PARTIAL_CONTENT",                 206, flags);
+    constructor.defineProperty("MULTI_STATUS",                    207, flags);
+    constructor.defineProperty("MULTIPLE_CHOICES",                300, flags);
+    constructor.defineProperty("MOVED_PERMANENTLY",               301, flags);
+    constructor.defineProperty("FOUND",                           302, flags);
+    constructor.defineProperty("SEE_OTHER",                       303, flags);
+    constructor.defineProperty("NOT_MODIFIED",                    304, flags);
+    constructor.defineProperty("USE_PROXY",                       305, flags);
+    constructor.defineProperty("SWITCH_PROXY",                    306, flags);
+    constructor.defineProperty("TEMPORARY_REDIRECT",              307, flags);
+    constructor.defineProperty("BAD_REQUEST",                     400, flags);
+    constructor.defineProperty("UNAUTHORIZED",                    401, flags);
+    constructor.defineProperty("PAYMENT_REQUIRED",                402, flags);
+    constructor.defineProperty("FORBIDDEN",                       403, flags);
+    constructor.defineProperty("NOT_FOUND",                       404, flags);
+    constructor.defineProperty("METHOD_NOT_ALLOWED",              405, flags);
+    constructor.defineProperty("NOT_ACCEPTABLE",                  406, flags);
+    constructor.defineProperty("PROXY_AUTHENTICATION_REQUIRED",   407, flags);
+    constructor.defineProperty("REQUEST_TIMEOUT",                 408, flags);
+    constructor.defineProperty("CONFLICT",                        409, flags);
+    constructor.defineProperty("GONE",                            410, flags);
+    constructor.defineProperty("LENGTH_REQUIRED",                 411, flags);
+    constructor.defineProperty("PRECONDITION_FAILED",             412, flags);
+    constructor.defineProperty("REQUEST_ENTITY_TOO_LARGE",        413, flags);
+    constructor.defineProperty("REQUEST_URI_TOO_LONG",            414, flags);
+    constructor.defineProperty("UNSUPPORTED_MEDIA_TYPE",          415, flags);
+    constructor.defineProperty("REQUESTED_RANGE_NOT_SATISFIABLE", 416, flags);
+    constructor.defineProperty("EXPECTATION_FAILED",              417, flags);
+    constructor.defineProperty("I_AM_A_TEAPOT",                   418, flags);
+    constructor.defineProperty("UNPROCESSABLE_ENTITY",            422, flags);
+    constructor.defineProperty("LOCKED",                          423, flags);
+    constructor.defineProperty("FAILED_DEPENDENCY",               424, flags);
+    constructor.defineProperty("UNORDERED_COLLECTION",            425, flags);
+    constructor.defineProperty("UPGRADE_REQUIRED",                426, flags);
+    constructor.defineProperty("RETRY_WITH",                      449, flags);
+    constructor.defineProperty("BLOCKED",                         450, flags);
+    constructor.defineProperty("INTERNAL_SERVER_ERROR",           500, flags);
+    constructor.defineProperty("NOT_IMPLEMENTED",                 501, flags);
+    constructor.defineProperty("BAD_GATEWAY",                     502, flags);
+    constructor.defineProperty("SERVICE_UNAVAILABLE",             503, flags);
+    constructor.defineProperty("GATEWAY_TIMEOUT",                 504, flags);
+    constructor.defineProperty("HTTP_VERSION_NOT_SUPPORTED",      505, flags);
+    constructor.defineProperty("VARIANT_ALSO_NEGOTIATES",         506, flags);
+    constructor.defineProperty("INSUFFICIENT_STORAGE",            507, flags);
+    constructor.defineProperty("BANDWIDTH_LIMIT_EXCEEDED",        509, flags);
+    constructor.defineProperty("NOT_EXTENDED",                    510, flags);
+  }
+
+
+  @Override public String getClassName() {
     return "Response";
   }
 
@@ -138,7 +201,7 @@ public class JSResponse
 
     return response;
   }
-  
+
   private Response response;
   private Scriptable headers;
 }
