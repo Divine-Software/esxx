5d0f04d272adda9d7364572ab915da88f7dc8d4d
Martin Blom <martin@blom.org>
2009-02-15 21:16:17 +0000

    Added JEE Servlet support.

---
+++ jee/org/esxx/ESXXServlet.java
@@ -0,0 +1,177 @@
+/*
+     ESXX - The friendly ECMAscript/XML Application Server
+     Copyright (C) 2007-2008 Martin Blom <martin@blom.org>
+
+     This program is free software: you can redistribute it and/or
+     modify it under the terms of the GNU General Public License
+     as published by the Free Software Foundation, either version 3
+     of the License, or (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package org.esxx;
+
+import java.util.Enumeration;
+import java.util.Properties;
+import java.io.*;
+
+import javax.servlet.http.*;
+import javax.servlet.*;
+
+import java.net.URI;
+import org.esxx.request.WebRequest;
+import org.esxx.request.ServletRequest;
+import org.esxx.util.IO;
+
+
+/** An HttpServlet that executes ESXX applications. */
+public class ESXXServlet extends HttpServlet {
+  public void init()
+    throws ServletException {
+    try {
+      Properties p = new Properties(System.getProperties());
+
+      // Copy esxx.* servlet init parameters to p
+      for (Enumeration e = getInitParameterNames(); e.hasMoreElements(); ) {
+	String name  = (String) e.nextElement();
+	String value = getInitParameter(name);
+
+	if (name.equals("esxx.app.include_path")) {
+	  StringBuilder sb = new StringBuilder();
+
+	  for (String path : value.split("\\|")) {
+	    if (sb.length() != 0) {
+	      sb.append(File.pathSeparator);
+	    }
+	  
+	    sb.append(resolvePath(path));
+	  }
+
+	  p.setProperty(name, sb.toString());
+	}
+	else if (name.startsWith("esxx.")) {
+	  p.setProperty(name, value);
+	}
+      }
+
+      root     = new File(resolvePath(getInitParameter("http-root"))).getCanonicalPath();
+      root_uri = new File(root).toURI();
+      esxx     = ESXX.initInstance(p);
+    }
+    catch (Exception ex) {
+      throw new ServletException("ESXXServlet.init() failed: " + ex.getMessage(), ex);
+    }
+  }
+
+
+  public void destroy() {
+  }
+
+
+  protected void service(HttpServletRequest sreq, HttpServletResponse sres)
+    throws ServletException, IOException {
+    try {
+      URI path_translated = root_uri.resolve(sreq.getServletPath().substring(1));
+      File file = new File(path_translated).getCanonicalFile();
+
+      if (!file.getPath().startsWith(root)) {
+	// Deny access to files outside the root
+	throw new FileNotFoundException("Document is outside root");
+      }
+      else {
+	File app_file = null;
+
+	if (file.exists()) {
+	  if (file.isDirectory()) {
+	    String listing = WebRequest.getFileListing(esxx, sreq.getRequestURI(), file);
+	    sres.setStatus(200);
+	    sres.setContentType("text/html; charset=UTF-8");
+	    sres.getWriter().print(listing);
+	  }
+	  else {
+	    if (WebRequest.fileTypeMap.getContentType(file).equals("application/x-esxx+xml")) {
+	      app_file = file;
+	    }
+	    else {
+	      sres.setStatus(200);
+	      sres.setContentType(WebRequest.fileTypeMap.getContentType(file));
+	      ServletRequest.setContentLength(sres, file.length());
+	      IO.copyStream(new FileInputStream(file), sres.getOutputStream());
+	    }
+	  }
+	}
+	else {
+	  // Find a file that do exists
+	  app_file = file;
+	  while (app_file != null && !app_file.exists()) {
+	    app_file = app_file.getParentFile();
+	  }
+
+	  if (app_file.isDirectory()) {
+	    throw new FileNotFoundException("Not Found");
+	  }
+
+	  if (!WebRequest.fileTypeMap.getContentType(app_file).equals("application/x-esxx+xml")) {
+	    throw new FileNotFoundException("Only ESXX files are directories");
+	  }
+	}
+
+	if (app_file != null) {
+	  ServletRequest sr = new ServletRequest(sreq, sres, root_uri, app_file);
+	  ESXX.Workload wl = esxx.addRequest(sr, sr, 0);
+	  sres = null;
+	  wl.future.get(); // Wait for request to complete
+	}
+      }
+    }
+    catch (Exception ex) {
+      int code = 500;
+      String title = "Internal Server Error";
+
+      if (ex instanceof FileNotFoundException) {
+	code = 404;
+	title = "Not Found";
+      }
+      else {
+	ex.printStackTrace(sres.getWriter());
+      }
+
+      sres.setStatus(code);
+      sres.setContentType("text/html; charset=UTF-8");
+      sres.getWriter().print(WebRequest.getHTMLHeader(esxx) +
+			     "<h2>" + title + "</h2>" +
+			     "<p>The requested resource "
+			     + WebRequest.encodeXMLContent(sreq.getRequestURI()) + " failed: " +
+			     WebRequest.encodeXMLContent(ex.getMessage()) +
+			     ".</p>" + WebRequest.getHTMLFooter(esxx));
+    }
+    finally {
+      if (sres != null) {
+	sres.flushBuffer();
+      }
+    }
+  }
+
+  private String resolvePath(String path) {
+    if (path == null) {
+      return getServletContext().getRealPath("");
+    }
+    else if (new File(path).isAbsolute()) {
+      return path;
+    }
+    else {
+      return getServletContext().getRealPath(path);
+    }
+  }
+
+  private String root;
+  private URI root_uri;
+  private ESXX esxx;
+}
---
+++ jee/org/esxx/request/ServletRequest.java
@@ -0,0 +1,139 @@
+/*
+     ESXX - The friendly ECMAscript/XML Application Server
+     Copyright (C) 2007-2008 Martin Blom <martin@blom.org>
+
+     This program is free software: you can redistribute it and/or
+     modify it under the terms of the GNU General Public License
+     as published by the Free Software Foundation, either version 3
+     of the License, or (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package org.esxx.request;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Enumeration;
+import java.util.Properties;
+import javax.servlet.http.*;
+import org.esxx.*;
+import org.mozilla.javascript.Context;
+
+public class ServletRequest
+  extends WebRequest {
+
+  public ServletRequest(HttpServletRequest sreq, 
+			HttpServletResponse sres,
+			URI root_uri, 
+			File canonical_script_file)
+    throws IOException, URISyntaxException {
+    super(canonical_script_file.toURI(), null, 
+	  createCGIEnvironment(sreq, root_uri, canonical_script_file),
+	  sreq.getInputStream(), 
+	  System.err, null);
+    this.sres = sres;
+  }
+
+  public Integer handleResponse(ESXX esxx, Context cx, Response response)
+    throws Exception {
+    try {
+      // Do not call super method, since it has a null OutputStream.
+
+      int  status = response.getStatus();
+
+      sres.setStatus(status);
+      sres.setContentType(response.getContentType(true));
+
+      response.enumerateHeaders(new Response.HeaderEnumerator() {
+  	  public void header(String name, String value) {
+  	    sres.addHeader(name, value);
+  	  }
+  	});
+
+      if ((status >= 100 && status <= 199) ||
+  	  status == 204 ||
+  	  status == 205 ||
+  	  status == 304) {
+	// No body
+      }
+      else {
+	if (response.isBuffered()) {
+	  // Output Content-Length header, if size is known
+	  setContentLength(sres, response.getContentLength(esxx, cx));
+	}
+
+	// Output body
+	response.writeResult(esxx, cx, sres.getOutputStream());
+      }
+
+      return 0;
+    }
+    catch (IOException ex) {
+      // If we fail to send response, it's probably just because
+      // nobody is listening anyway.
+      return 20;
+    }
+    catch (Exception ex) {
+      ex.printStackTrace();
+      throw ex;
+    }
+    finally {
+      sres.flushBuffer();
+    }
+  }
+
+
+  public static void setContentLength(HttpServletResponse sres, long length) {
+    if(length <= Integer.MAX_VALUE){
+      sres.setContentLength((int) length);
+    }else{
+      sres.addHeader("Content-Length", Long.toString(length));
+    }  
+  }
+
+  private static Properties createCGIEnvironment(HttpServletRequest sreq,
+						 URI root_uri, 
+						 File canonical_script_file) 
+    throws URISyntaxException {
+
+    URI full_request_uri = new URI(sreq.getScheme(),
+				   null,
+				   sreq.getServerName(),
+				   sreq.getServerPort(),
+				   sreq.getRequestURI(),
+				   sreq.getQueryString(),
+				   null);
+			       
+    Properties p = createCGIEnvironment(sreq.getMethod(), sreq.getProtocol(), 
+					full_request_uri,
+					new InetSocketAddress(sreq.getLocalAddr(), 
+							      sreq.getLocalPort()),
+					new InetSocketAddress(sreq.getRemoteAddr(), 
+							      sreq.getRemotePort()),
+					sreq.getContextPath() + "/",
+					root_uri, canonical_script_file);
+
+    // Add request headers
+
+    for (Enumeration e = sreq.getHeaderNames(); e.hasMoreElements(); ) {
+      String h = (String) e.nextElement();
+      p.setProperty(ESXX.httpToCGI(h), sreq.getHeader(h));
+    }
+
+    return p;
+  }
+
+  private HttpServletResponse sres;
+}
