8d7e8a0e1b5e92926f5d32d3c6d1bced207f3402
Martin Blom <martin@blom.org>
2009-03-22 12:30:24 +0000

    Added esxx.bat.

--- package/esxx-js.bat
+++ package/esxx-js.bat
@@ -1,2 +1,2 @@
-java -Desxx.app.include_path="%~dp0\share;%~dp0\share\site" -jar "%~dp0\esxx.jar" --script -- %*
+@java -Desxx.app.include_path="%~dp0\share;%~dp0\share\site" -jar "%~dp0\esxx.jar" --script -- %*
 
---
+++ package/esxx.bat
@@ -0,0 +1,1 @@
+@java -Desxx.app.include_path="%~dp0\share;%~dp0\share\site" -jar "%~dp0\esxx.jar" %*
