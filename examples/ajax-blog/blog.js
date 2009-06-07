7862cc285843f7dbeec886b5ff3ffd2d76911482
Martin Blom <martin@blom.org>
2009-06-07 18:10:28 +0000

    The Ajax Blog and app-form-sql examples now uses an in-memory database by default.

--- examples/ajax-blog/blog.js
+++ examples/ajax-blog/blog.js
@@ -4,4 +4,4 @@ esxx.include("src/Blog.js");
 XML.ignoreWhitespace = false;
 XML.prettyPrinting   = false;
 
-var blog = new Blog("jdbc:h2:Blog", "admin", "admin");
+var blog = new Blog("jdbc:h2:mem:Blog;DB_CLOSE_DELAY=-1", "admin", "admin");
