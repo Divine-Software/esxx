ad0132dcc42037219a39cad37a27de546dab3aa5
Martin Blom <martin@blom.org>
2009-11-25 22:09:01 +0000

    Added subcommands.

--- tools/generate-help/generate-help.js
+++ tools/generate-help/generate-help.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env esxx-js
 
+esxx.include('esxx/harmony.js');
+
 const err = java.lang.System.err;
 const out = java.lang.System.out;
 
@@ -9,54 +11,54 @@ XML.ignoreWhitespace = false;
 var db;
 var wordIDs = {};
 
-function main(prog, db_files) {
-  if (arguments.length != 2) {
-    err.println("Usage: " + prog + " <db-path>");
+function main(prog, cmd, db_files) {
+  if (arguments.length < 3) {
+    err.println("Usage: " + prog
+		+ " <clear-db|index-db|zip-db|query|add-mdc|add-berlios> <db-path> [<arg1> ...]");
     return 10;
   }
 
   db = new URI("jdbc:h2:" + db_files);
 
-  db.query("drop table docs if exists");
-  try { db.query("drop table words"); } catch (ignored) {}
-  try { db.query("drop table doc_words"); } catch (ignored) {}
+  let rc = 0;
 
-  db.query("create table docs"
-	   + "("
-	       + "id      identity not null,"
-	       + "section varchar not null,"
-	       + "title   varchar not null,"
-	       + "text    varbinary not null,"
-	       + "uri     varchar not null,"
-	       + "primary key(id),"
-	       + "unique (section, title),"
-	       + "unique (uri)"
-	   + ")");
+  if (cmd == "clear-db") {
+    db.query("drop table docs if exists");
 
-  db.query("create table words"
-	   + "("
-	       + "id   identity not null,"
-	       + "word varchar not null,"
-	       + "primary key(id),"
-	       + "unique (word)"
-	   + ")");
+    db.query("create table docs"
+	     + "("
+		 + "id	    identity not null,"
+		 + "section varchar not null,"
+		 + "title   varchar not null,"
+		 + "text    varbinary not null,"
+		 + "uri	    varchar not null,"
+		 + "primary key(id),"
+		 + "unique (section, title),"
+		 + "unique (uri)"
+	     + ")");
+  }
+  else if (cmd == "index-db") {
+    rc = recreate_index();
+  }
+  else if (cmd == "zip-db") {
+    db.query("backup to 'esxx-help.zip'");
+  }
+  else if (cmd == "query") {
+    rc = query(Array.splice(arguments, 3));
+  }
+  else if (cmd == "dump-mdc") {
+    rc = dump_book("https://developer.mozilla.org/en/Core_JavaScript_1.5_Reference", dump_mdc);
+  }
+  else {
+    err.println("Unknown command: " + cmd);
+    return 10;
+  }
 
-  db.query("create table doc_words"
-	   +"("
-	      + "word_id integer not null,"
-	      + "doc_id  integer not null,"
-	      + "section boolean not null,"
-	      + "foreign key (word_id) references words(id) on delete cascade,"
-	      + "foreign key (doc_id)  references docs(id) on delete cascade"
-	   + ")");
-
-  dump_book("https://developer.mozilla.org/en/Core_JavaScript_1.5_Reference", dump_mdc);
-
-  return 0;
+  return rc;
 }
 
 function dump_book(base, dump_page) {
-  let visited    = {};
+  let visited	 = {};
   let candidates = [base];
 
   while (candidates.length > 0) {
@@ -76,6 +78,85 @@ function dump_book(base, dump_page) {
   return 0;
 }
 
+
+function recreate_index() {
+  db.query("drop table words if exists");
+  db.query("drop table doc_words if exists");
+
+  db.query("create table words"
+	     + "("
+		 + "id	 identity not null,"
+		 + "word varchar not null,"
+		 + "primary key(id),"
+		 + "unique (word)"
+	     + ")");
+
+  db.query("create table doc_words"
+	     +"("
+		+ "word_id integer not null,"
+		+ "doc_id  integer not null,"
+		+ "title boolean not null,"
+		+ "foreign key (word_id) references words(id) on delete cascade,"
+		+ "foreign key (doc_id)	 references docs(id) on delete cascade"
+	     + ")");
+
+  for each (let entry in db.query("select id, section, title from docs").entry) {
+    create_index(parseInt(entry.id), entry.section.toString(), entry.title.toString());
+  }
+
+  return 0;
+}
+
+function query(terms) {
+  let final_docs;
+
+  for each (let term in terms) {
+    term = term.toLowerCase();
+
+    let docs = db.query("select distinct dw.doc_id "
+			+ "from words w "
+			+ "inner join doc_words dw on dw.word_id = w.id "
+			+ "where w.word = {0} "
+			+ "order by doc_id",
+			[term]);
+
+    docs = [parseInt(id) for each (id in docs.entry.doc_id)];
+
+    if (!final_docs) {
+      final_docs = docs;
+    }
+    else {
+      final_docs = docs.filter(function (v) { return final_docs.indexOf(v) != -1; });
+    }
+  }
+
+  if (final_docs.length == 0) {
+    out.println("No documents matched the given terms.");
+    return 5;
+  }
+  else if (final_docs.length > 1) {
+    out.println("The following documents matched the given terms. Please be more specific.");
+
+    let matches = db.query("select section, title from docs where id in ({docs})", {
+      docs: final_docs
+    });
+
+    for each (let match in matches.entry) {
+      out.println(match.section + "." + match.title);
+    }
+
+    return 5;
+  }
+  else {
+    out.println(db.query("select utf8tostring(expand(text)) as text from docs where id = {0}",
+			 final_docs).entry.text);
+    return 0;
+  }
+}
+
+
+// Helper functions
+
 function dump_mdc(url) {
   err.println("Dumping " + url);
 
@@ -85,7 +166,7 @@ function dump_mdc(url) {
 
   let sect  = breadcrumb[breadcrumb.length() - 2].toString();
   let title = page..h1.(@id == "title").toString();
-  let toc   = page..div.(@id == "pageToc");
+//  let toc   = page..div.(@id == "pageToc");
   let text  = page..div.(@id == "pageText");
 
   if (breadcrumb.length() > 2 /* Prevent redirects to MDC / Mozilla Developer Center */
@@ -97,9 +178,8 @@ function dump_mdc(url) {
 		    <title>{title}</title>
 		  </head>
 		  <body>
-		    {toc}
 		    {text}
-	 	  </body>
+		  </body>
 		</html>);
   }
 
@@ -112,7 +192,7 @@ function import_html(url, section, title, html) {
   try {
     new URI("mdc.tmp.html").save(html);
 
-    let p = java.lang.Runtime.getRuntime().exec(["/bin/sh", 
+    let p = java.lang.Runtime.getRuntime().exec(["/bin/sh",
 						 "-c",
 						 "links -assume-codepage utf8 -dump "
 						 + "mdc.tmp.html > mdc.tmp.txt"]);
@@ -122,40 +202,41 @@ function import_html(url, section, title, html) {
 
     let text = new URI("mdc.tmp.txt").load("text/plain");
 
-    let doc_id = db.query("insert into docs (section, title, text, uri) "
-			  + "values ({0}, {1}, compress(stringtoutf8({2}), 'deflate'), {3})", 
-			  [section, title, text, url]).entry.identity;
-    doc_id = parseInt(doc_id);
-
-    add_words(doc_id, section, true);
-    add_words(doc_id, title, false);
+    db.query("insert into docs (section, title, text, uri) "
+	     + "values ({0}, {1}, compress(stringtoutf8({2}), 'deflate'), {3})",
+	     [section, title, text, url]);
   }
   finally {
     new URI("mdc.tmp.html").remove();
     new URI("mdc.tmp.txt").remove();
   }
-
 }
 
-function add_words(id, str, is_section) {
-  let words = str.split(/[^a-zA-Z0-9.-]+/);
+function create_index(id, section, title) {
+  let delim = /[^-.a-zA-Z0-9]|\.+/; // Split on non-word chars or '...', but keep '1.5'
+  let path  = section + "." + title;
+  let words = path.split(delim);
 
+  words.push(path);
+  words.push(section);
+  words.push(title);
+
+  add_words(id, words, true);
+}
+
+function add_words(id, words, is_title) {
   for each (let word in words) {
-    word = word.toLowerCase();
+    word = word.toLowerCase().trim();
 
-    if (!wordIDs.hasOwnProperty(word)) {
-      wordIDs[word] = parseInt(db.query("insert into words (word) values ({0})", 
-					[word]).entry.identity);
+    if (word !== "") {
+      if (!wordIDs.hasOwnProperty(word)) {
+	wordIDs[word] = parseInt(db.query("insert into words (word) values ({0})",
+					  [word]).entry.identity);
+      }
+
+      db.query("insert into doc_words (word_id, doc_id, title) "
+	       + "values ({0}, {1}, {2})",
+	       [wordIDs[word], id, is_title]);
     }
-
-    db.query("insert into doc_words (word_id, doc_id, section) "
-	     + "values ({0}, {1}, {2})", 
-	     [wordIDs[word], id, is_section]);
   }
 }
-
-// select d.section, d.title
-// from words w
-// inner join doc_words dw on w.id = dw.word_id 
-// inner join docs d on dw.doc_id = d.id
-// where w.word = 'object'
