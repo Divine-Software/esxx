1002607978ef4f3227981ce2387d3f7649057fed
Martin Blom <martin@blom.org>
2009-12-03 14:53:53 +0000

    Added initial SQL testcase and a test runner.

---
+++ tests/run-tests.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env esxx-js
+
+esxx.include("esxx/Test.js");
+
+const testRunner = new TestRunner();
+
+// Load testcase modules
+for each (let file in new URI(".").load().file.(/^testmod-.*\.js$/.test(name)).@uri) {
+  esxx.include(file);
+}
+
+function main() {
+  return testRunner.run();
+}
---
+++ tests/testmod-uri-jdbc.js
@@ -0,0 +1,74 @@
+
+testRunner.add(new TestCase({
+  name: "testmod-uri-jdbc",
+
+  setUp: function() {
+    this.db = new URI("jdbc:h2:mem:testmod-uri-jdbc");
+    this.db.query("create table test (id identity, string varchar, number int)");
+  },
+
+  tearDown: function() {
+    this.db.query("drop all objects delete files");
+  },
+
+  testQueryInsertJS: function() {
+    let one = this.db.query("insert into test (string, number) values ({0}, {1})", 
+			    ["one", 1]);
+    let two = this.db.query("insert into test values "
+			    + "(default, {s1}, {n1}), (default, {s2}, {n2})", 
+			    { s1: "two", n1: 2,
+			      s2: "three", n2: 3 });
+
+    Assert.that(one.entry.length() == 1, "INSERT did not generate one single entry")
+    Assert.that(one..identity.length() == 1, "INSERT did not generate one single identity")
+
+    Assert.areEqual(one.entry.@updateCount, 1, "updateCount is not 1");
+    Assert.areEqual(one.entry.identity, 1, "IDENTITY of first INSERT was not 1");
+
+    Assert.areEqual(two.entry.@updateCount, 2, "updateCount is not 2");
+    Assert.areEqual(two.entry.identity, 3, "IDENTITY of second INSERT was not 3");
+  },
+
+  testQueryInsertXML: function() {
+    let one = this.db.query("insert into test (string, number) values ({0}, {1})", 
+			    <><e>one</e><e>1</e></>);
+    let two = this.db.query("insert into test values "
+			    + "(default, {s1}, {n1}), (default, {s2}, {n2})", 
+			    <elem>
+			    <s1>two</s1>   <n1>2</n1>
+			    <s2>three</s2> <n2>3</n2>
+			    </elem>
+			    );
+
+    Assert.that(one.entry.length() == 1, "INSERT did not generate one single entry")
+    Assert.that(one..identity.length() == 1, "INSERT did not generate one single identity")
+
+    Assert.areEqual(one.entry.@updateCount, 1, "updateCount is not 1");
+    Assert.areEqual(one.entry.identity, 1, "IDENTITY of first INSERT was not 1");
+
+    Assert.areEqual(two.entry.@updateCount, 2, "updateCount is not 2");
+    Assert.areEqual(two.entry.identity, 3, "IDENTITY of second INSERT was not 3");
+  },
+
+  testQuerySelect: function() {
+    this.db.query("insert into test values (default, 'one', 1)");
+    this.db.query("insert into test values (default, 'two', 2)");
+
+    let one   = this.db.query("select number from test where string = {one}", { one: "one" });
+    let two   = this.db.query("select string from test where id = {0}", [2]);
+    let multi = this.db.query("select string from test where id = 1 ;" +
+			      "select number from test");
+
+    Assert.that(one.entry.length() == 1, "SELECT did not return exactly one entry");
+    Assert.areEqual(one.entry.number, 1, "SELECT did not return 1");
+
+    Assert.that(two.entry.length() == 1, "SELECT did not return exactly one entry");
+    Assert.areEqual(two.entry.string, "two", "SELECT did not return 'two'");
+
+//    Assert.that(multi.entry.length() == 3, "SELECT did not return exactly three entries");
+//    Assert.areEqual(multi.entry[0].string, "one", "SELECT #1 did not return 'one'");
+//    Assert.areEqual(multi.entry[1].string, "one", "SELECT #2 did not return 'one'");
+//    Assert.areEqual(multi.entry[2].string, "two", "SELECT #2 did not return 'two'");
+  },
+
+}));
