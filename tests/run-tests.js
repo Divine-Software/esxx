3ab324fbb09890fce2e91990bf1805192b5955d7
Martin Blom <martin@blom.org>
2010-03-15 20:40:57 +0000

    tests/run-tests.js can now optionally take files to run from the command line.

--- tests/run-tests.js
+++ tests/run-tests.js
@@ -4,11 +4,20 @@ esxx.include("esxx/Test.js");
 
 const testRunner = new TestRunner();
 
-// Load testcase modules
-for each (let file in new URI(esxx.location, ".").load().file.(/^testmod-.*\.js$/.test(name)).@uri) {
-  esxx.include(file);
-}
+function main(prog, file1) {
+  if (arguments.length > 1) {
+    // Load specified testcase modules
+    for each (let file in Array.slice(arguments, 1)) {
+      esxx.include(new URI(esxx.wd, file));
+    }
+  }
+  else {
+    // Load all testcase modules
+    for each (let file in new URI(esxx.location, ".").load()
+	      .file.(/^testmod-.*\.js$/.test(name)).@uri) {
+      esxx.include(file);
+    }
+  }
 
-function main() {
   return testRunner.run();
 }
