d7ebf290c34fb8bc66d9b38d7b3facf06cff3fc3
Martin Blom <martin.blom@onslip.com>
2012-03-07 22:10:36 +0100

    The windows installer now includes version number in the file name.

--- make_package.bat
+++ make_package.bat
@@ -8,10 +8,7 @@ cd /d "%BUILD%"
 
 call ant -buildfile "%SOURCE%\build.xml" -Dbuild.dir="%BUILD%" -Dprefix=/ -Ddocdir=/doc -Ddatadir=/share -DDESTDIR="%BUILD%\root" generate-build-files install
 
-"%WIX%bin\heat" dir root -var var.RootDir -cg ESXXFiles -gg -scom -sreg -sfrag -srd  -dr INSTALLLOCATION -o ESXXFiles.wxs
-"%WIX%bin\candle" -dRootDir=root ESXX.wxs ESXXFiles.wxs
-"%WIX%bin\light" -ext WixUIExtension -cultures:en-us ESXX.wixobj ESXXFiles.wixobj -o ESXX.msi
-
+call run_wix.bat
 copy esxx*.msi "%SOURCE%"
 
 cd /d "%SOURCE%"
